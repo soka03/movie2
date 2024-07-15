67cecb571af9b91028514d0df49e82d5d5fccc10
soka03 <soka27@naver.com>
2024-07-16 02:38:12 +0900

    cors

--- community/settings.py
+++ community/settings.py
@@ -72,6 +72,8 @@ ROOT_URLCONF = 'community.urls'
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
     "http://127.0.0.1:8000",
     "https://minihackton.store"]
 
